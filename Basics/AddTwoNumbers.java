6829ab227357ce30771d1e185e5353fbed23dc21
Shree3030 <twinklingstart1@gmail.com>
2025-08-02 15:41:59 +0530

    Add Two numbers

---
+++ Basics/AddTwoNumbers.java
@@ -0,0 +1,9 @@
+class AddTwoNumbers {
+    static int addition(int a, int b) {
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+          System.out.println(addition(2,3));
+    }
+}
